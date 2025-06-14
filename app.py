035668980499c9f3b15a95a55268d771c65f4b36
decade92 <long.svnh4rum@gmail.com>
2025-06-15 00:30:37 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import cv2
 import numpy as np
 
 ASSETS_DIR = os.path.join(os.path.dirname(__file__), "assets")
-LOGO_PATH = os.path.join(ASSETS_DIR, "logo.png")
+LOGO_PATH = os.path.join(ASSETS_DIR, "logo_bidv.png")
 FONT_PATH = os.path.join(ASSETS_DIR, "Roboto-Bold.ttf")
 BG_PATH = os.path.join(ASSETS_DIR, "background.png")
 
@@ -164,25 +164,17 @@ st.markdown(font_css, unsafe_allow_html=True)
 # Giao diện người dùng
 st.title("🇻🇳 Tạo ảnh VietQR đẹp chuẩn NAPAS ")
 
-
-
-# Đọc ảnh logo BIDV
-with open("assets/logo_bidv.png", "rb") as f:
-    logo_data = base64.b64encode(f.read()).decode()
-
-# Hiển thị tiêu đề với logo
 st.markdown(
     f"""
-    <div style="display: flex; align-items: center; gap: 12px; margin-bottom: 16px;">
-        <img src="data:image/png;base64,{logo_data}" style="height:30px; width:auto;">
-        <span style="font-family: Roboto, sans-serif; font-weight: bold; font-size:24px; color:#007C71;">
+    <div style="display: flex; align-items: center;">
+        <img src="data:image/png;base64,{base64.b64encode(open(LOGO_PATH, "rb").read()).decode()}" style="max-height:25px; height:25px; width:auto; margin-right:10px;">
+        <span style="font-family: Roboto, sans-serif; font-weight: bold; font-size:25px; color:#007C71;">
             Dành riêng cho BIDV Thái Bình - PGD Tiền Hải
         </span>
     </div>
     """,
     unsafe_allow_html=True
 )
-
 
 
 # === FORM NHẬP DỮ LIỆU ===
@@ -192,6 +184,14 @@ st.header("📥 Nhập tay hoặc phân tích từ ảnh QR")
 for field in ["account", "bank_bin", "name", "note", "amount"]:
     if field not in st.session_state:
         st.session_state[field] = ""
+
+
+# Khởi tạo giá trị mặc định để tránh lỗi nếu không có input
+merchant_id = ""
+bank_bin = ""
+acc_name = ""
+add_info = ""
+amount = ""
 
 uploaded_file = st.file_uploader("📤 Tải ảnh QR VietQR", type=["png", "jpg", "jpeg"])
 if uploaded_file:
