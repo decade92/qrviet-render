3c39385960b8f8ba5b729eb3215efff7dbf9bb51
decade92 <long.svnh4rum@gmail.com>
2025-06-15 00:31:52 +0700

    Update app.py

--- app.py
+++ app.py
@@ -164,11 +164,15 @@ st.markdown(font_css, unsafe_allow_html=True)
 # Giao diện người dùng
 st.title("🇻🇳 Tạo ảnh VietQR đẹp chuẩn NAPAS ")
 
+with open("assets/logo_bidv.png", "rb") as f:
+    logo_data = base64.b64encode(f.read()).decode()
+
+# Hiển thị tiêu đề với logo
 st.markdown(
     f"""
-    <div style="display: flex; align-items: center;">
-        <img src="data:image/png;base64,{base64.b64encode(open(LOGO_PATH, "rb").read()).decode()}" style="max-height:25px; height:25px; width:auto; margin-right:10px;">
-        <span style="font-family: Roboto, sans-serif; font-weight: bold; font-size:25px; color:#007C71;">
+    <div style="display: flex; align-items: center; gap: 12px; margin-bottom: 16px;">
+        <img src="data:image/png;base64,{logo_data}" style="height:30px; width:auto;">
+        <span style="font-family: Roboto, sans-serif; font-weight: bold; font-size:24px; color:#007C71;">
             Dành riêng cho BIDV Thái Bình - PGD Tiền Hải
         </span>
     </div>
